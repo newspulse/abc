66e7118ad47de1f9f86d39a86a553f5def4b6f2e
Lauren Rui <lauren.rui@salespreso.com>
2016-07-30 21:34:51 +1000

    feat(Logo): Add to bottom right

--- js/components/App.js
+++ js/components/App.js
@@ -1,12 +1,13 @@
 import {connect as Cerebral} from "cerebral-view-react";
 import React from "react";
-import {DataGraph} from "./DataGraph.js";
+import DataGraph from "./DataGraph.js";
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div className="app">
 				{"Hai"}
+				<div className="np-logo"></div>
 				<DataGraph />
 			</div>
 		);
