6962977e0fd07c1e31a974533dd600131c613323
Liz Honing <liz.honing@salespreso.com>
2016-07-30 12:33:29 +1000

    feat(map): add initial map

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 console.log("HELLO WORLD");
+
+window.initMap = function() {
+	var mapDiv = document.getElementById('map');
+	var map = new google.maps.Map(mapDiv, {
+		center: {lat: -27.68, lng: 134.78},
+		zoom: 4
+	});
+}
+
+//initMap();
+
+// /ReactDOM.render(<Layout controller={controller} />, document.getElementById("spui"));
